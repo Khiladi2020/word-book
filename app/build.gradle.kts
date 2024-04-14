069a28870110e107eda8544097d073a3563a4e30
Ravinder Singh <ravindermaan0123@gmail.com>
2024-04-14 22:34:32 +0530

    chore: added dagger with ksp used ksp instead of kapt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -36,11 +37,15 @@ android {
 }
 
 dependencies {
+    val dagger_version = "2.50"
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.dagger:dagger:$dagger_version")
+    ksp("com.google.dagger:dagger-compiler:$dagger_version")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
