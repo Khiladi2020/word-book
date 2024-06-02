af943e28dddd71d9970cd0433d4bf44e9a41c46a
Ravinder Singh <ravindermaan0123@gmail.com>
2024-06-02 18:44:03 +0530

    chore: handle back press in app bar

--- app/src/main/java/com/example/wordbook/ui/SettingsFragment.kt
+++ app/src/main/java/com/example/wordbook/ui/SettingsFragment.kt
@@ -1,7 +1,29 @@
 package com.example.wordbook.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.wordbook.R
+import com.example.wordbook.databinding.FragmentSettingsBinding
 
-class SettingsFragment: Fragment(R.layout.fragment_settings) {
+class SettingsFragment: Fragment() {
+    private lateinit var binding: FragmentSettingsBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        initListeners()
+        return binding.root
+    }
+
+    private fun initListeners() {
+        binding.topAppBar.setNavigationOnClickListener {
+            view?.findNavController()?.navigateUp()
+        }
+    }
 }
