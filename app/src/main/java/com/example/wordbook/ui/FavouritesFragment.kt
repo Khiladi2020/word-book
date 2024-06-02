75b3bddce9455e1c28db6c746a7b3067231ba9af
Ravinder Singh <ravindermaan0123@gmail.com>
2024-06-02 19:03:01 +0530

    feat: add app bar to favourites & hostory fragments

--- app/src/main/java/com/example/wordbook/ui/FavouritesFragment.kt
+++ app/src/main/java/com/example/wordbook/ui/FavouritesFragment.kt
@@ -1,7 +1,29 @@
 package com.example.wordbook.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.wordbook.R
+import androidx.navigation.findNavController
+import com.example.wordbook.databinding.FragmentFavouritesBinding
 
-class FavouritesFragment: Fragment(R.layout.fragment_favourites) {
+class FavouritesFragment: Fragment() {
+    private lateinit var binding: FragmentFavouritesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentFavouritesBinding.inflate(inflater, container,false)
+        initListeners()
+        return binding.root
+    }
+
+    private fun initListeners(){
+        binding.topAppBar.setNavigationOnClickListener {
+            view?.findNavController()?.navigateUp()
+        }
+    }
 }
--- app/src/main/java/com/example/wordbook/ui/HistoryFragment.kt
+++ app/src/main/java/com/example/wordbook/ui/HistoryFragment.kt
@@ -1,7 +1,29 @@
 package com.example.wordbook.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.wordbook.R
+import androidx.navigation.findNavController
+import com.example.wordbook.databinding.FragmentHistoryBinding
 
-class HistoryFragment: Fragment(R.layout.fragment_history) {
+class HistoryFragment: Fragment() {
+    private lateinit var binding: FragmentHistoryBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHistoryBinding.inflate(inflater, container,false)
+        initListeners()
+        return binding.root
+    }
+
+    private fun initListeners(){
+        binding.topAppBar.setNavigationOnClickListener {
+            view?.findNavController()?.navigateUp()
+        }
+    }
 }
