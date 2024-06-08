495d9b1b7169104b7ce05de9486ea17fc0eccd99
Ravinder Singh <ravindermaan0123@gmail.com>
2024-06-09 01:44:56 +0530

    chore: fix back button in detail fragment

--- app/src/main/java/com/example/wordbook/ui/DetailFragment.kt
+++ app/src/main/java/com/example/wordbook/ui/DetailFragment.kt
@@ -1,7 +1,29 @@
 package com.example.wordbook.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.wordbook.R
+import com.example.wordbook.databinding.FragmentDetailBinding
 
-class DetailFragment: Fragment(R.layout.fragment_detail) {
+class DetailFragment : Fragment() {
+    private lateinit var binding: FragmentDetailBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentDetailBinding.inflate(inflater, container, false)
+        initListeners()
+        return binding.root
+    }
+
+    private fun initListeners() {
+        binding.topAppBar.setNavigationOnClickListener {
+            view?.findNavController()?.navigateUp()
+        }
+    }
 }
