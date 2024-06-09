eb577bfc83501dca67aca2c9ad2f012029e2e230
Ravinder Singh <ravindermaan0123@gmail.com>
2024-06-10 00:38:37 +0530

    chore: pass context in main application using app component factory

--- app/src/main/java/com/example/wordbook/MainApplication.kt
+++ app/src/main/java/com/example/wordbook/MainApplication.kt
@@ -3,13 +3,14 @@ package com.example.wordbook
 import android.app.Application
 import com.example.wordbook.di.AppComponent
 import com.example.wordbook.di.DaggerAppComponent
+//import com.example.wordbook.di.DaggerAppComponent
 import dagger.Component
 
 class MainApplication: Application() {
     lateinit var appComponent: AppComponent
     override fun onCreate() {
         super.onCreate()
-        appComponent = DaggerAppComponent.create()
+        appComponent = DaggerAppComponent.factory().create(context = applicationContext)
     }
 
 }
