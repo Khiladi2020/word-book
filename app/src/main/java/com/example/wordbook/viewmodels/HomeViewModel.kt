0e8cc71e96496b7f26b18fa840164ae120ca04cd
Ravinder Singh <ravindermaan0123@gmail.com>
2024-07-07 15:41:55 +0530

    feat: optimize the word search by add debounce

--- app/src/main/java/com/example/wordbook/viewmodels/HomeViewModel.kt
+++ app/src/main/java/com/example/wordbook/viewmodels/HomeViewModel.kt
@@ -8,6 +8,8 @@ import com.example.wordbook.MainActivity.Companion.TAG
 import com.example.wordbook.model.SearchItemModel
 import com.example.wordbook.model.Word
 import com.example.wordbook.model.WordRepository
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -17,13 +19,20 @@ import kotlinx.coroutines.launch
 class HomeViewModel(val wordRepository: WordRepository): ViewModel() {
 
     private val _wordsList: MutableStateFlow<List<Word>> = MutableStateFlow(listOf())
+    private var searchJob: Job? = null
+
 
     // Public
     val wordsList = _wordsList.asStateFlow()
 
     fun updateSearchText(searchText: String){
-        viewModelScope.launch {
-            Log.d(TAG,"Repository request started")
+        if(searchText.isEmpty()) return
+        // Cancel old search Job
+        searchJob?.cancel()
+
+        searchJob = viewModelScope.launch {
+            delay(DEBOUNCE_PERIOD_MS)
+            Log.d(TAG,"Repository request started for text $searchText")
             val result = wordRepository.getWordsStartingWith(searchText)
             Log.d(TAG, "THis is my latest data ${result.size} $result")
             _wordsList.emit(result)
@@ -31,6 +40,7 @@ class HomeViewModel(val wordRepository: WordRepository): ViewModel() {
     }
 
     companion object {
+        const val DEBOUNCE_PERIOD_MS = 200L
         class Factory(val wordRepository: WordRepository): ViewModelProvider.Factory {
             override fun <T : ViewModel> create(modelClass: Class<T>): T {
                 return HomeViewModel(wordRepository) as T
